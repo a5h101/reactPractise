ca0c632823087d55a549ba335095b9c0f66c4961
Akash Singh <113436653+a5h101@users.noreply.github.com>
2023-01-02 16:49:26 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
+// import './App.css';
+
+// function App() {
+//   const fName = 'Akash';
+//   const lName = 'Singh';
+//   const num = 9;
+//   return (
+//     <div className="App">
+//       <h1>{fName + ' ' + lName}'s Fav Food: </h1>
+//       <ul>
+//         <li>Rajma</li>
+//         <li>Raita</li>
+//         <li>Pav bhaji</li>
+//       </ul>
+//       <h3>
+//         <i>Your lucky number is {num}</i>
+//       </h3>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
 
 function App() {
   const fName = 'Akash';
   const lName = 'Singh';
-  const num = 9;
+  const num = new Date();
   return (
     <div className="App">
-      <h1>{fName + ' ' + lName}'s Fav Food: </h1>
-      <ul>
-        <li>Rajma</li>
-        <li>Raita</li>
-        <li>Pav bhaji</li>
-      </ul>
-      <h3>
-        <i>Your lucky number is {num}</i>
-      </h3>
+      <p>Created by {fName + ' ' + lName}</p>
+      <p>
+        <i>Copyright {num.getFullYear()}</i>
+      </p>
     </div>
   );
 }
